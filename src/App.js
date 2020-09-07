a69628b8b7df2ea2c8df2408d3b2f93b093f21b1
Vikra Ardiansyah <vikraardi@gmail.com>
2020-09-07 23:54:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  Nav,
+  NavbarText,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+} from "reactstrap";
+import Logo from "./assets/logo.svg";
+import Bars from "./assets/bars.png";
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar light expand="md" className=".navbar">
+        <Container fluid="lg" className="container">
+          <NavbarBrand>
+            <img src={Logo} alt="logo" className="logo" />
+          </NavbarBrand>
+          <img src={Bars} className="ml-auto bar" alt="logo" onClick={toggle} />
+          <Collapse isOpen={isOpen} className="nav-item" navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavbarText className="nav-text">Tentang Kami</NavbarText>
+              </NavItem>
+              <NavItem>
+                <NavbarText className="nav-text">Semua Outlet</NavbarText>
+              </NavItem>
+              <NavItem>
+                <NavbarText className="nav-text">Promo</NavbarText>
+              </NavItem>
+              <NavItem>
+                <NavbarText className="nav-text">Karier</NavbarText>
+              </NavItem>
+              <NavItem>
+                <NavbarText className="nav-text-download">
+                  Download App
+                </NavbarText>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+      <Jumbotron className="jumbotron">
+        <Container fluid="lg" className="container">
+          <Row>
+            <Col lg="6">
+              <p className="text-question">Kamu lapar dan haus?</p>
+              <p className="text-h1">
+                Tenang... ada Hangry! yang siap mengatasi
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
+    </>
   );
-}
+};
 
 export default App;
